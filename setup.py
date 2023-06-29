7616cb360fb74b4395489e17220f3a0b312b2345
Björn Benouarets <70456959+bbenouarets@users.noreply.github.com>
2023-06-29 10:33:55 +0200

    Fix import

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fibertrace",
-    version="0.1.0",
+    version="0.1.1",
     author="ByteSentinel.io",
     author_email="dev@bytesentinel.io",
     description="A library for simple logging",
